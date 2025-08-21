6fbae4598a3cccf4675d3700f4427371548c4c89
PhenixP02 <plpayton01@indianatech.net>
2025-08-21 14:27:17 -0400

    CS1300 Homework 1 Word Document and Programs

---
+++ Two Number Subtraction Calculator.py
@@ -0,0 +1,9 @@
+# Phenix Payton
+# 8/21/25
+# Two Number Subtraction Calculator V1.0
+# Takes two User Inputed Numbers, Subtracts the First number by the Second and outputs Difference
+num1 = int(input("Choose initial number : "))
+num2 = int(input("Choose Number to Subtract from First: "))
+numTotal = int(num1 - num2)
+print(num1, " - ", num2, " = ", numTotal)
+print("The Total is: ", numTotal)
